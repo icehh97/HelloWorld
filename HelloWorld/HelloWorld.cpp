5bbe8f669c1b7fd2ebf9ac2e98db099421cc5535
icehh97 <stan97heo@gmail.com>
2025-09-02 16:55:27 +0900

    gg

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -16,6 +16,8 @@ shift + 화살표 : 블럭 잡기
 이거 맨끝에 꺼 그 사이 주석으로 만들기 ㅇㅇ
 alt+화살표 위 아래 : 줄 바꾸기 
 ctrl+화살표 단어 단위
+ctrl+k+c 주석 설정
+ctrl+k+u 주석해제
 */
 
 
@@ -46,6 +48,37 @@ ctrl(alt)+B+B : 프로젝트(솔루션) 빌드
 //   4. [오류 목록] 창을 사용하여 오류를 봅니다.
 //   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
 //   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+
+
+
+/*
+변수(Variable)
+- 변하는 숫자
+- 컴퓨터에 값을 기억 시키기 위해 만들고 사용 
+*/
+
+/*
+연산자(operator)
+꼐산을 하기 위한 기호
+- 대입 연산자
+= 일상적인 연산 ? 
+= 오른쪽에 있는 값을 왼쪽에 넣어라 
+a=b; //b에 있는 값을 a에 넣어라
+오른쪽 변수 자체는 변하지 않음
+
+- 산술 연산자
+사칙연산
++더하기-빼기*곱하기/나누기 %까지 모듈러연산=나머지 연산
+a=5%3;
+a에는 2가 들어간다.(5를 3으로 나누었을 때 나머지가 2)
+-복합 대입연산자
+줄여쓰기 용도
+a+=b; //a=a+b;
+a++=// a에다가 1을 더해라
+a--= // a에다가 1을 빼라
+*/
+
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream> //INPUT, OUTPUT STREAM
 #include <stdio.h>//C 언어에서 쓰는 전통적인 방식.?
@@ -56,11 +89,11 @@ ctrl(alt)+B+B : 프로젝트(솔루션) 빌드
 
 int main() //엔트리 포인트 (코드가 시작되는 곳) // {열은거 있으면 닫아야함}
 {
-    
-    
-    printf("hello\tworld!\n" );//c언어의 출력방법
-    printf( "HJ\t\"\t" );//c언어의 출력방법 ""안에 \n이 있으면 줄을 바꾸라는 의미 (이스케이프 스퀀스)
-    printf("97년생입니다.\n"); 
+
+
+    printf("hello\tworld!\n");//c언어의 출력방법
+    printf("HJ\t\"\t");//c언어의 출력방법 ""안에 \n이 있으면 줄을 바꾸라는 의미 (이스케이프 스퀀스)
+    printf("97년생입니다.\n");
 
     //이스케이프 스퀀스
     // \n : 줄바꾸기
@@ -68,29 +101,185 @@ int main() //엔트리 포인트 (코드가 시작되는 곳) // {열은거 있�
     // \" : 쌍따옴표 한개
     // \\ : \찍기
     // \r : 캐리지 리턴(윈도우에서는 중요하지 않음 , 리눅스에서는 중요함) - 제일 앞으로 보내기
-    
-   
+
+
     printf("안녕하세요\n");
     printf("식사하셨나요\n");
     printf("이거 안쓰면 어떻게 되는 거\n");
     printf("나이가 몇 살이신가요?\n");
 
-   
-    int number = 0;  //변수
+
+    int number = 0;  //변수 //number라는 이름을 가진 int(인티저)변수를 선언하고 거기에 0을 대입한다(full-explain)
+
+    //number = 5.3; number 에는 5가 저장된다
+    //number = 5.3
+        //number = 5 / 2; //number에는 2가 저장된다
+
+    //int bumber; // 변수선언 int한 순간에 넘버라는 자리 생김
+    // int : 정수형 (소수점이 없는 숫자, 5, 10, -2, 0, etc..)
+    // 
+    // 
+    //number = 0; // 변수에 값을 대입 넘버통해 값을 쓰거나 읽음 (선언후에 사용 가능 반드시 선언 먼저 )
+
+
     scanf("%d", &number); //녹색밑줄 잘못 쓴 것은 아닌데 문제가 될 수 있는 것 //위험한 함수 메모리 침범 가능 (visual studio 에서 권장 x)
 
 
     printf("입력한 숫자는(C) : %d\n", number); //저장된 숫자가 대신 들어감 숫자를 하나 입력 받기(C)
-    
+
     printf(" %d\n이군요", number);
 
     std::cin >> number;
     printf("입력한 숫자는(C++) : %d\n", number); //숫자를 하나 입력 받기 (C++)
 
-    printf("%d이구나",number);
-  
-
+    printf("%d이구나", number);
     //프로그램을 실행했을 때 나이를 물어보고 입력받은 숫자를 그대로 출력
+
+
+
+    
+
+    int number1 = 0; //변수는 선언 즉시 값을 넣어주세요
+    int number2 = 0;
+
+    printf("숫자 1을 입력하시오 : ");
+    std::cin >> number1;
+
+    printf("숫자 2을 입력하시오 : ");
+    std::cin >> number2;
+
+    printf("두 숫자의 합은 %d입니다.", number1 + number2);
+
+    int number3 = number1 + number2;
+    printf("두 숫자의 합은 %d 입니다.\n", number3);
+
+
+
+    /*printf 문 이용해서
+    *
+    ***
+        *****
+        찍기*/
+
+
+        //특수문자 이용 미로 모양 출력
+    printf("    *\n   ***\n  *****\n");
+    printf("ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ\nㅁㅁㅁㅁㅁㅁㅁ\nㅁㅁㅁㅁ\n");
+    int h = 7;
+    int k = 22;
+    printf("h+k = %d\n", h + k);
+
+    //파스칼 케이스를 써야한다네
+
+
+   // 두 수를 입력 받아 스왑하기
+    int w, s, temp; //w랑 s랑 temp(임시)를 지정 한 거 ?
+    printf("w를 입력해주세요");
+    printf("w 입력: "); //w에 대한 숫자를 입력하라는 거 ?
+    scanf("%d", &w);//이건 w 값을 찾는 건가 ?
+
+    printf("s를 입력해주세요");
+    printf("s 입력: "); //이제 이게 s에 대한 숫자를 입력하라는 거 ?
+    scanf("%d", &s); //이건 s값을 찾는건가 ? 
+
+    printf("두 숫자를 바꾸겠습니다");
+    temp = w;
+    w = s;
+    s = temp;
+
+
+    printf("w=%d, s=%d\n", w, s); //이건 w랑 s 값을 나열하는 거 ?
+
+
+
+
+    //사각형의 가로, 세로를 입력 받아 넓이를 출력하기
+    //현재 사용 알파멧 hk ws
+    int n, m;
+    printf("n을 입력해주세요");//s값을 입력하고
+    printf("n입력:");
+    scanf("%d", &n); //이걸 정수 값으로 정의하는 건가 ?
+    printf("입력한숫자 n = %d\n", n);
+
+    printf("m을 입력해주세요");
+    printf("m 입력:");
+    scanf("%d", &m);
+    printf("입력한 숫자 M = %d\n", m);
+
+    printf("n은 가로 m은 새로입니다");
+    printf("넓이를 계산해보겠습니다.");
+    printf("%d*%d=%d\n", n, m, n * m);
+
+
+
+
+    // 두 수를 입력 받아 나머지를 출력하기 
+
+    int z, x;
+
+    printf("몫z을 입력해주세요 : ");
+    scanf("%d", &z);
+
+
+
+    printf("나누는 수 x를 입력해주세요 :");
+    scanf("%d", &x);
+
+
+
+    int quotient=z / x;
+    int   remainder=z - (quotient * x);
+
+
+   
+
+    printf("몫= %d, 나머지=%d\n", quotient, remainder );
+
+
+
+
+    //시간 계산기
+    //-초를 입력 받아 시분초 출력하기
+
+
+
+    int hour, minute, second;
+    printf("hour를 입력해주세요(0~23) : ");
+    scanf("%d", &hour);
+   while (hour < 0 || hour > 23)
+    {
+        printf("잘못된 hour 값입니다!\n");
+        printf("다시 입력: ");
+        scanf("%d", &hour);
+    }
+    printf("minute을 입력해주세요(0~59) : ");
+    scanf("%d", &minute);
+
+    while (minute < 0 || minute > 59)
+    {
+        printf("잘못된 minute 값입니다!\n");
+        printf("다시 입력: ");
+        scanf("%d", &minute);
+    }
+    printf("second를 입력해주세요(0~59) : ");
+    scanf("%d", &second);
+
+    while (second < 0 || second > 59)
+    {
+        printf("잘못된 second 값입니다!\n");
+        printf("다시 입력: ");
+        scanf("%d", &second);
+    }
+
+   
+
+    printf("%d시%d분%d초\n", hour, minute, second);
+
+
+
+   //동전 개수 계산하기 = 금액을 받아 동전 각각 몇 개 필요한지 계산 , 큰 동전 최대한 많이
+    
+
 
 //std: printf("hello my world!|n"); //C언어의 출력방법
     //std::cout << "Hello My World!\n"; //이것은 한 줄 출력하기 //C++출력 방법 //빨간 줄은 문법 오류 //언리얼에서 사용X printf 사용
@@ -108,7 +297,6 @@ int main() //엔트리 포인트 (코드가 시작되는 곳) // {열은거 있�
 //return 0; 끝내기 ㅇㅇ
 //한 번 해보죠
 //변화가 저장되어 있음 파일전체는 아님
-
 
 
 
